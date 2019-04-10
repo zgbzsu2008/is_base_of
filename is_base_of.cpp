ad9aec7202bfbe34fb785fa9479b911d32f816cc
Zhang Guo Bin <zgbzsu2008@163.com>
2019-04-10 13:27:15 +0800

    v1.1

--- is_base_of.cpp
+++ is_base_of.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <type_traits>
 
-struct A {
-  using key = A;
-};
-
+class A {};
 class B : public A {};
+class C : public A {};
 
 int main() {
   std::cout << std::boolalpha;
+  std::cout << "A is base of A? " << std::is_base_of<A, A>::value << '\n';
+  std::cout << "B is base of C? " << std::is_base_of<B, C>::value << '\n';
   std::cout << "A is base of B? " << std::is_base_of<A, B>::value << '\n';
-  std::cout << "A::key is base of A? " << std::is_base_of<A::key, A>::value
-            << '\n';
   return 0;
 }
